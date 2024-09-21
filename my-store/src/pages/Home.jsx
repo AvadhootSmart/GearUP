affbbdbe96e3f922b34a011115c41d59f56cf8fd
AvadhootSmart <1234avadhootsmart@gmail.com>
2024-09-21 09:32:50 +0530

    domain update

--- my-store/src/pages/Home.jsx
+++ my-store/src/pages/Home.jsx
@@ -7,7 +7,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PageLoader from "./PageLoader";
 
-const BACKEND_DOMAIN = "http://localhost:5000";
+const BACKEND_DOMAIN = "https://av-gaming-gear-be.vercel.app";
 
 const Home = () => {
     const [cardData, setCardData] = useState([{}]);
@@ -33,42 +33,42 @@ const Home = () => {
         <>
             <div>
                 <PageLoader />
-                <div >
-                <div className="h-screen w-full bg-[#23232f]">
-                    <HeroProduct cards={cardData} />
-                </div>
-                <div className="min-h-screen w-full overflow-hidden bg-[#23232f] pb-40">
-                    <h1 className="w-full py-10 text-center font-[Poppins] text-5xl text-white">
-                        Categories
-                    </h1>
-                    <div className="flex h-screen w-[100vw] flex-wrap">
-                        <CategoriesCard
-                            image="/Keyboard.png"
-                            category="Keyboard"
-                            price="49"
-                        />
-                        <CategoriesCard image="/Mouse.png" category="Mouse" price="29" />
-                        <CategoriesCard
-                            image="/Headset.png"
-                            category="Headset"
-                            price="22"
-                        />
-                        <CategoriesCard
-                            image="/Headset.png"
-                            category="Headset"
-                            price="22"
-                        />
+                <div>
+                    <div className="h-screen w-full bg-[#23232f]">
+                        <HeroProduct cards={cardData} />
                     </div>
-                </div>
-                <div className="Page3 min-h-screen w-full overflow-hidden bg-[#23232f] py-10">
-                    <div className="heading px-14 font-[Poppins] text-5xl font-bold text-white">
-                        <h1 className="-mb-14">Featured</h1>
+                    <div className="min-h-screen w-full overflow-hidden bg-[#23232f] pb-40">
+                        <h1 className="w-full py-10 text-center font-[Poppins] text-5xl text-white">
+                            Categories
+                        </h1>
+                        <div className="flex h-screen w-[100vw] flex-wrap">
+                            <CategoriesCard
+                                image="/Keyboard.png"
+                                category="Keyboard"
+                                price="49"
+                            />
+                            <CategoriesCard image="/Mouse.png" category="Mouse" price="29" />
+                            <CategoriesCard
+                                image="/Headset.png"
+                                category="Headset"
+                                price="22"
+                            />
+                            <CategoriesCard
+                                image="/Headset.png"
+                                category="Headset"
+                                price="22"
+                            />
+                        </div>
                     </div>
-                    <Featured featuredCards={featuredCards} />
-                    <ToastContainer />
+                    <div className="Page3 min-h-screen w-full overflow-hidden bg-[#23232f] py-10">
+                        <div className="heading px-14 font-[Poppins] text-5xl font-bold text-white">
+                            <h1 className="-mb-14">Featured</h1>
+                        </div>
+                        <Featured featuredCards={featuredCards} />
+                        <ToastContainer />
+                    </div>
                 </div>
             </div>
-        </div >
         </>
     );
 };
