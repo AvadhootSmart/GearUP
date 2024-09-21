8c101596d2b871fc7403cb9554604eac8cc37592
AvadhootSmart <1234avadhootsmart@gmail.com>
2024-09-21 18:07:12 +0530

    fixed cors :(

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.DEV_DOMAIN,
+        origin: process.env.FRONTEND_DOMAIN,
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
