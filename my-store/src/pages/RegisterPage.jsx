a5fe1f32c2cef4199f64ecfe26f78bbc25bcb7d1
AvadhootSmart <1234avadhootsmart@gmail.com>
2024-11-03 16:20:55 +0530

    updated Auth UI

--- my-store/src/pages/RegisterPage.jsx
+++ my-store/src/pages/RegisterPage.jsx
@@ -35,12 +35,15 @@ function RegisterPage() {
     e.preventDefault();
 
     try {
-      const response = await axios.post(`${import.meta.env.VITE_BACKEND_DOMAIN}/register`, {
-        username,
-        name,
-        email,
-        password,
-      });
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_DOMAIN}/register`,
+        {
+          username,
+          name,
+          email,
+          password,
+        },
+      );
 
       if (response.status === 200) {
         dispatch(loginSuccess(response.data));
@@ -64,57 +67,99 @@ function RegisterPage() {
           </h1>
         </div>
         <div className="relative z-10 flex h-screen w-full items-center justify-center">
-          <div className="FormCard flex h-[70vh] w-[35vw]  items-center  rounded-2xl border-2 backdrop-blur-lg ">
+          <div className="FormCard flex  w-[35vw] items-center rounded-2xl border-2 p-10 backdrop-blur-lg ">
             <form
               onSubmit={register}
-              className=" flex h-full w-full flex-grow flex-col items-center"
+              className="flex h-full w-full flex-col items-center justify-center gap-4"
             >
-              <h1 className="pt-14 font-[Poppins] text-4xl font-extrabold text-white">
+              <h1 className="font-[Poppins] text-4xl font-extrabold text-white">
                 Create an Account
               </h1>
-              <input
-                type="text"
-                name="Username"
-                value={username}
-                onChange={(e) => setusername(e.target.value)}
-                placeholder="Enter your username"
-                className="mt-6 h-8 w-[65%] rounded-md border-2 border-black p-2 font-[Poppins] "
-              />
-              <input
-                type="text"
-                name="Name"
-                value={name}
-                onChange={(e) => setname(e.target.value)}
-                placeholder="Enter your name"
-                className="mt-6 h-8 w-[65%] rounded-md border-2 border-black p-2 font-[Poppins]"
-              />
-              <input
-                type="text"
-                name="Email"
-                value={email}
-                onChange={(e) => setemail(e.target.value)}
-                placeholder="Enter your email"
-                className="mt-6 h-8 w-[65%] rounded-md border-2 border-black p-2 font-[Poppins]"
-              />
-              <input
-                type="password"
-                name="Password"
-                value={password}
-                onChange={(e) => setpassword(e.target.value)}
-                placeholder="Enter your password"
-                className="mt-8 h-8 w-[65%] rounded-md border-2 border-black p-2 font-[Poppins]"
-              />
-              <button className="mt-10 w-[60%] rounded-2xl bg-[#676eff] font-[Poppins] text-2xl">
-                Register
-              </button>
-              <div className="mt-8 flex gap-2 text-xl text-[#72748e]">
-                <h3>Have an account already??</h3>
-                <Link
-                  to="/Login"
-                  className="text-[#676eff] hover:text-[#313690]"
+
+              <div className="flex w-full flex-col gap-2">
+                <label
+                  htmlFor="username"
+                  className="font-[Poppins] text-xl text-white"
                 >
-                  Log In
-                </Link>
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="Username"
+                  value={username}
+                  onChange={(e) => setusername(e.target.value)}
+                  className="block w-full rounded-lg border bg-gray-500 p-2.5 text-sm text-white  placeholder:text-gray-300 focus:border-[#676eff]"
+                  placeholder="Enter your username"
+                />
+              </div>
+              {/* Name */}
+              <div className="flex w-full flex-col gap-2">
+                <label
+                  htmlFor="name"
+                  className="font-[Poppins] text-xl text-white"
+                >
+                  Name
+                </label>
+
+                <input
+                  type="text"
+                  name="Name"
+                  value={name}
+                  onChange={(e) => setname(e.target.value)}
+                  placeholder="Enter your name"
+                  className="block w-full rounded-lg border bg-gray-500 p-2.5 text-sm text-white  placeholder:text-gray-300 focus:border-[#676eff]"
+                />
+              </div>
+              {/* email */}
+              <div className="flex w-full flex-col gap-2">
+                <label
+                  htmlFor="name"
+                  className="font-[Poppins] text-xl text-white"
+                >
+                  Email
+                </label>
+
+                <input
+                  type="text"
+                  name="Email"
+                  value={email}
+                  onChange={(e) => setemail(e.target.value)}
+                  placeholder="Enter your email"
+                  className="block w-full rounded-lg border bg-gray-500 p-2.5 text-sm text-white  placeholder:text-gray-300 focus:border-[#676eff]"
+                />
+              </div>
+              {/* password */}
+              <div className="flex w-full flex-col gap-2">
+                <label
+                  htmlFor="name"
+                  className="font-[Poppins] text-xl text-white"
+                >
+                  Password
+                </label>
+
+                <input
+                  type="password"
+                  name="Password"
+                  value={password}
+                  onChange={(e) => setpassword(e.target.value)}
+                  placeholder="Enter your password"
+                  className="block w-full rounded-lg border bg-gray-500 p-2.5 text-sm text-white  placeholder:text-gray-300 focus:border-[#676eff]"
+                />
+              </div>
+
+              <div className="flex items-center justify-between gap-2 w-full">
+                <div className="flex gap-2 text-xl text-[#72748e]">
+                  <h3>Have an account already??</h3>
+                  <Link
+                    to="/Login"
+                    className="text-[#676eff] hover:text-[#313690]"
+                  >
+                    Log In
+                  </Link>
+                </div>
+                <button className="self-end rounded-lg bg-[#676eff] px-3 py-1 font-[Poppins]  text-2xl">
+                  Register
+                </button>
               </div>
             </form>
           </div>
